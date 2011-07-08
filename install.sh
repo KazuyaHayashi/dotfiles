01b50d647f696fb2beca4db2db5978839de8df45
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2011-07-09 06:37:37 +0900

    add write_log function.

--- install.sh
+++ install.sh
@@ -4,10 +4,17 @@
 export CUR_DIR=`pwd`
 # const
 INSTALLER=install.sh
+LOG=.dotfiles_install.log
+
+function write_log()
+{
+    echo $@ >> ~/${LOG}
+}
 
 for directory in `ls ${CUR_DIR}`
 do
     if [ -d ${CUR_DIR}/${directory} ]; then
+        write_log "${CUR_DIR}/${directory}/${INSTALLER}"
         sh ${CUR_DIR}/${directory}/${INSTALLER}
     fi
 done
