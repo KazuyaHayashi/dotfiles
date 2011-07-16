97d2fc72f41bf1f32e1d7fdd5d14f322c169d571
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2011-07-17 06:06:52 +0900

    元の .bashrc にあった環境変数の設定を引き継げるようにした

--- bash/install.sh
+++ bash/install.sh
@@ -3,6 +3,8 @@
 #
 # function
 #
+
+# .bashrc のバックアップを行う
 function backup()
 {
     if [ -e ~/${BACKUP_DIR} ]; then
@@ -19,9 +21,22 @@ function setup()
     ln -s ${CUR_DIR}/bash/bashrc ~/.bashrc
 }
 
+# 元の .bashrc に設定されている環境変数を取り出し
+# 置き換えた bashrc で読み込めるようにする
+function extract_exports()
+{
+    if [ -e ~/.bashrc ]; then
+        if [ -L ~/.bashrc ]; then
+            return 1
+        fi
+        grep ^export ~/.bashrc > ~/.bash_exports_origin
+    fi
+}
+
 #
 # main
 #
+extract_exports
 backup
 echo "install bashrc"
 setup
